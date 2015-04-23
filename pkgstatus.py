dc105ac635f39d20acb9d3600d0ac5d2221592d1
Bryan Drewery <bryan@shatow.net>
2015-04-23 18:42:11 -0500

    Use just /builds for listing builds, not /sets,/servers,/types

--- pkgstatus.py
+++ pkgstatus.py
@@ -1,6 +1,6 @@
 import datetime
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask.ext.pymongo import PyMongo
 import flask.ext.pymongo as pymongo
@@ -50,7 +50,35 @@ def create_app():
                 latest_builds=latest_builds,
                 servers=get_server_map())
 
-    @app.route('/build/<buildid>')
+    @app.route('/builds')
+    def builds():
+        query = {}
+        for key, value in request.args.iteritems():
+            query[key] = value
+        if "setname" in query:
+            if query['setname'] == "default":
+                query['setname'] = ''
+        if "all" in query:
+            del(query['all'])
+            latest = False
+        else:
+            query['latest'] = True
+            latest = True
+        if "build_types" in query:
+            build_types = query['build_types']
+        else:
+            build_types = ["package", "qat", "exp"]
+        latest_builds = {}
+        for build_type in build_types:
+            query['type'] = build_type
+            latest_builds[build_type] = get_builds(query)
+        return render_template('index.html',
+                latest=latest,
+                servers=get_server_map(),
+                build_types=build_types,
+                latest_builds=latest_builds)
+
+    @app.route('/builds/<buildid>')
     def build(buildid):
         build = mongo.db.builds.find_one_or_404({'_id': buildid})
         ports = mongo.db.ports.find_one({'_id': buildid})
@@ -65,64 +93,6 @@ def create_app():
                 build=build,
                 ports=ports,
                 servers=get_server_map())
-
-    @app.route('/sets/<setname>')
-    def sets(setname):
-        if setname == "default":
-            setname = ""
-        build_types = ["package", "qat", "exp"]
-        latest_builds = {}
-        for build_type in build_types:
-            latest_builds[build_type] = get_builds({
-                "latest": True,
-                'type': build_type,
-                "setname": setname})
-        return render_template('index.html',
-                latest=True,
-                servers=get_server_map(),
-                build_types=build_types,
-                latest_builds=latest_builds)
-
-    @app.route('/servers/<server>')
-    def servers(server):
-        build_types = ["package", "qat", "exp"]
-        latest_builds = {}
-        for build_type in build_types:
-            latest_builds[build_type] = get_builds({
-                "latest": True,
-                'type': build_type,
-                "server": server})
-        return render_template('index.html',
-                latest=True,
-                servers=get_server_map(),
-                build_types=build_types,
-                latest_builds=latest_builds)
-
-    @app.route('/types/<build_type>')
-    def types(build_type):
-        latest_builds = {}
-        latest_builds[build_type] = get_builds({
-            "latest": True,
-            'type': build_type})
-
-        return render_template('index.html',
-                latest=True,
-                servers=get_server_map(),
-                build_types=[build_type],
-                latest_builds=latest_builds)
-
-    @app.route('/builds/<buildname>')
-    def builds(buildname):
-        build_types = ["package", "qat", "exp"]
-        latest_builds = {}
-        for build_type in build_types:
-            latest_builds[build_type] = get_builds({
-                'type': build_type,
-                "buildname": buildname})
-        return render_template('index.html',
-                servers=get_server_map(),
-                build_types=build_types,
-                latest_builds=latest_builds)
 
     return app
 
